11c4edbffabd27dd80d95246d35e47e813fae18d
sohaib-Kr <krimsohaib518@gmail.com>
2025-03-19 10:22:16 -0700

    installing astro codebase

--- courses/utiles/index.mjs
+++
@@ -1,5 +0,0 @@
-export {arrow} from './arrow.mjs'
-export {createDynamicText,latex,createDynamicLatex} from './text.mjs'
-export {fadeNextStep,fadeText,shakeAnimation,fadeBounce} from './effects.mjs'
-export {colorConfig} from './colorConfig.mjs'
-export {textStyles} from './textConfig.mjs'
